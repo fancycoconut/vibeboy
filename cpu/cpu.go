21f9885030f000b724c6043b263733666b92c3d5
Kawai Wong <kawai.wong@pushpay.com>
2025-05-29 15:04:25 +1200

    Add various registers to the CPU

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -2,15 +2,29 @@ package cpu
 
 // CPU represents the Gameboy CPU (Sharp LR35902)
 type CPU struct {
-	// Registers, flags, etc. will go here
+	// 8-bit registers
+	A, F byte // Accumulator & Flags
+	B, C byte
+	D, E byte
+	H, L byte
+
+	// 16-bit registers
+	SP uint16 // Stack Pointer
+	PC uint16 // Program Counter
+
+	// Internal state (add more as needed)
 }
 
 // New returns a new CPU instance
 func New() *CPU {
-	return &CPU{}
+	return &CPU{
+		// Gameboy starts with PC at 0x0100, SP at 0xFFFE (typical)
+		PC: 0x0100,
+		SP: 0xFFFE,
+	}
 }
 
-// Step executes a single CPU instruction
-func (c *CPU) Step() {
-	// Instruction execution logic will go here
+// Step executes a single CPU instruction (to be implemented)
+func (c *CPU) Step(mem interface{}) {
+	// Fetch-decode-execute logic will go here
 }
